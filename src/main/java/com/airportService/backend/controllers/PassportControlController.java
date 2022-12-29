78b844f2426d6a300051f5a6cf1a035fee627f04
Olha Balumatkinnnna <balumatkinaolga@gmail.com>
2022-12-29 17:41:42 +0200

    added "PassportControlController" and "SecurityControlController" to backend-controllers folder

---
+++ src/main/java/com/airportService/backend/controllers/PassportControlController.java
@@ -0,0 +1,38 @@
+package com.airportService.backend.controllers;
+
+import com.airportService.backend.models.PassportControl;
+import com.airportService.backend.services.PassportControlServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class PassportControlController {
+    @Autowired
+    private final PassportControlServiceImpl passportControlService;
+
+    public PassportControlController(PassportControlServiceImpl passportControlService) {
+        this.passportControlService = passportControlService;
+    }
+
+    public void addRecord(PassportControl passportControl) {
+        passportControlService.addRecord(passportControl);
+    }
+
+    public List<PassportControl> findAll() {
+        return passportControlService.findAll();
+    }
+
+    public void updateComments(Long id, String comments) {
+        passportControlService.updateComments(id, comments);
+    }
+
+    public void updateToChecked(Long id) {
+        passportControlService.updateToChecked(id);
+    }
+
+    public List<PassportControl> getRecordByPassport(String passportNumber) {
+        return passportControlService.getRecordByPassport(passportNumber);
+    }
+}
---
+++ src/main/java/com/airportService/backend/controllers/SecurityControlController.java
@@ -0,0 +1,38 @@
+package com.airportService.backend.controllers;
+
+import com.airportService.backend.models.SecurityControl;
+import com.airportService.backend.services.SecurityControlServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class SecurityControlController {
+    @Autowired
+    private final SecurityControlServiceImpl securityControlService;
+
+    public SecurityControlController(SecurityControlServiceImpl securityControlService) {
+        this.securityControlService = securityControlService;
+    }
+
+    public void addRecord(SecurityControl securityControl) {
+        securityControlService.addRecord(securityControl);
+    }
+
+    public List<SecurityControl> findAll() {
+        return securityControlService.findAll();
+    }
+
+    public void updateComments(Long id, String comments) {
+        securityControlService.updateComments(id, comments);
+    }
+
+    public void updateToChecked(Long id) {
+        securityControlService.updateToChecked(id);
+    }
+
+    public List<SecurityControl> getRecordByPassport(String passportNumber) {
+        return securityControlService.getRecordByPassport(passportNumber);
+    }
+}
