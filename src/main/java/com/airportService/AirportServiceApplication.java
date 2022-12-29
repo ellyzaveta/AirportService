849fcf42c11b659a25c27691589bf0c4eb663aa9
lizakhmyz <lizamari21@gmail.com>
2022-12-29 19:47:08 +0200

    fixed

--- src/main/java/com/airportService/AirportServiceApplication.java
+++ src/main/java/com/airportService/AirportServiceApplication.java
@@ -1,5 +1,7 @@
 package com.airportService;
 
+import com.airportService.backend.data.DataFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -13,9 +15,23 @@ public class AirportServiceApplication {
         SpringApplication.run(AirportServiceApplication.class, args);
     }
 
+    @Autowired
+    private DataFactory dataFactory;
+
     @EventListener(ApplicationReadyEvent.class)
     private void testJpaMethods() throws IOException {
-
+//        dataFactory.init();
+//        dataFactory.addFlight(1);
+//        dataFactory.addFlight(1);
+//        dataFactory.addFlight(0);
+//        dataFactory.addFlight(0);
+//        dataFactory.addFlight(0);
+//        dataFactory.addFlight(0);
+//        dataFactory.addFlight(0);
+//        dataFactory.addFlight(0);
+//        dataFactory.addFlight(0);
+        // 1 - generate rand flight with departure time in the next 2 hours
+        // 0 - generate rand flight
     }
 }
 
